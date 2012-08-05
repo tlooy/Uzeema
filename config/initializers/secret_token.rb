3fef1567b03c1b98b780bd8fe189fcc60d65f0e6
Tom Looy <tlooy24@gmail.com>
2012-08-05 09:36:57 -0700

    My Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uzeema::Application.config.secret_token = '3bca7da80cd426245b72c5352a3bb4e5fa6419e69ddda14b3d1741f500dc505689f19134448ab309dfb092f24583c5989f2250fdc88fcab4d1e950f9a66de1f3'
